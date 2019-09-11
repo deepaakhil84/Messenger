63e21b2998b4aab16ef9d69e090db8b9905a1dea
deepaakhil84 <deepaakhiltvm@gmail.com>
2019-09-11 16:31:10 +0100

    styling

--- Frontend/src/Component/Userform.js
+++ Frontend/src/Component/Userform.js
@@ -25,69 +25,59 @@ class Userform extends Component {
 
   render() {
     return (
-      <div className="userform-container mt-5 shadow d-flex justify-content-center">
-        <form className="userform" onSubmit={this.handleSubmit}>
-          <table class="table">
-            <h1 className="mb-5 font-weight-light">USER FORM</h1>
-            <tr>
-              <div className="d-flex flex-row justify-content-around">
-                <td>FirstName</td>
-                <input
-                  type="text"
-                  name="firstName"
-                  className="form-control "
-                  placeholder="Enter Firstname"
-                  value={this.state.firstName}
-                  onChange={this.handleChange}
-                />
-              </div>
-            </tr>
-            <tr>
-              <div className="d-flex flex-row justify-content-between">
-                <td>LastName</td>
+      <div className="container">
+        <form className="registration-form" onSubmit={this.handleSubmit}>
+          <h1 className="text-center">USER FORM</h1>
+          <div className="form-group">
+            <td>FirstName</td>
+            <input
+              type="text"
+              name="firstName"
+              className="form-control "
+              placeholder="Enter Firstname"
+              value={this.state.firstName}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <td>LastName</td>
 
-                <input
-                  type="text"
-                  name="lastName"
-                  className="form-control "
-                  placeholder="Enter lastname"
-                  value={this.state.lastname}
-                  onChange={this.handleChange}
-                />
-              </div>
-            </tr>
-            <tr>
-              <div className="d-flex flex-row justify-content-between">
-                <td>Email</td>
-                <input
-                  type="email"
-                  name="email"
-                  className="form-control "
-                  placeholder="Enter your email"
-                  value={this.state.email}
-                  onChange={this.handleChange}
-                />
-              </div>
-            </tr>
-            <tr>
-              <div className="d-flex flex-row justify-content-between">
-                <td>password</td>
-                <input
-                  type="password"
-                  name="password"
-                  className="form-control "
-                  placeholder="Enter your password"
-                  value={this.state.password}
-                  onChange={this.handleChange}
-                />
-              </div>
-            </tr>
-            <div className="form-group d-flex justify-content-center">
-              <button type="submit" class="btn btn-success">
-                Submit{" "}
-              </button>
-            </div>
-          </table>
+            <input
+              type="text"
+              name="lastName"
+              className="form-control "
+              placeholder="Enter lastname"
+              value={this.state.lastname}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <td>Email</td>
+            <input
+              type="email"
+              name="email"
+              className="form-control "
+              placeholder="Enter your email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <td>Password</td>
+            <input
+              type="password"
+              name="password"
+              className="form-control "
+              placeholder="Enter your password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="form-group d-flex justify-content-center">
+            <button type="submit" class="btn btn-success">
+              Submit{" "}
+            </button>
+          </div>
         </form>
       </div>
     );
