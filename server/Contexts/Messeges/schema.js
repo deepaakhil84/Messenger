8ae8c629702b95905f87acd71cd92d9fe9a8ea10
deepaakhil84 <deepaakhiltvm@gmail.com>
2019-10-18 16:34:44 +0100

    displayed the users in the sidebar

--- server/Contexts/Messeges/schema.js
+++ server/Contexts/Messeges/schema.js
@@ -1,16 +1,17 @@
-import { Schema, model } from 'mongoose'
-
-​const messageSchema = new Schema(
+import { Schema, model } from "mongoose";
+const messageSchema = new Schema(
   {
-
-   senderId: String,
+    senderId: String,
     reciverId: String,
-    senderName:String,
-    message:String
-},
-  { timestamps: { createdAt: true, updatedAt: true } }
-)
-​
-const MessageModel = model('message', messageSchema)
-​
-export default MessageModel
+    senderName: String,
+    message: String
+  },
+  {
+    timestamps: {
+      createdAt: true,
+      updatedAt: true
+    }
+  }
+);
+const MessageModel = model("message", messageSchema);
+export default MessageModel;
